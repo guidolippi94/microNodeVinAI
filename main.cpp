0799478abbb31bde61d3433394383315dc72e7a2
Guido Lippi <guidolippi94@users.noreply.github.com>
2020-04-14 17:11:29 +0200

    Versione  caricata su micro nodo Frascole

--- main.cpp
+++ main.cpp
@@ -1,94 +1,194 @@
 #include <Arduino.h>
 
-#include "Adafruit_Sensor.h"
-#include "Adafruit_AM2320.h"
-#include <WiFi.h>
-#include <HTTPClient.h>
+
+// Your GPRS credentials (leave empty, if not needed)
+const char apn[]      = "TM"; // APN (example: internet.vodafone.pt) use https://wiki.apnchanger.org
+const char gprsUser[] = ""; // GPRS User
+const char gprsPass[] = ""; // GPRS Password
+
+// SIM card PIN (leave empty, if not defined)
+const char simPIN[]   = ""; 
+
+// Server details
+// The server variable can be just a domain name or it can have a subdomain. It depends on the service you are using
+const char server[] = "vin-ai.com"; // domain name: example.com, maker.ifttt.com, etc
+const char resource[] = "/post-test";         // resource path, for example: /post-data.php
+const int  port = 1111;                             // server port number
+
+// TTGO T-Call pins
+#define MODEM_RST            5
+#define MODEM_PWKEY          4
+#define MODEM_POWER_ON       23
+#define MODEM_TX             27
+#define MODEM_RX             26
+#define I2C_SDA              21
+#define I2C_SCL              22
+
+// Set serial for debug console (to Serial Monitor, default speed 115200)
+#define SerialMon Serial
+// Set serial for AT commands (to SIM800 module)
+#define SerialAT Serial1
+
+// Configure TinyGSM library
+#define TINY_GSM_MODEM_SIM800      // Modem is SIM800
+#define TINY_GSM_RX_BUFFER   1024  // Set RX buffer to 1Kb
+
+// Define the serial console for debug prints, if needed
+//#define DUMP_AT_COMMANDS
+
+#include <Wire.h>
+#include <TinyGsmClient.h>
+
+#ifdef DUMP_AT_COMMANDS
+  #include <StreamDebugger.h>
+  StreamDebugger debugger(SerialAT, SerialMon);
+  TinyGsm modem(debugger);
+#else
+  TinyGsm modem(SerialAT);
+#endif
+
+//DHT settings
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+
+#define DHTPIN 4     // Digital pin connected to the DHT sensor
+#define DHTTYPE    DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+// I2C for SIM800 (to keep it running when powered from battery)
+TwoWire I2CPower = TwoWire(0);
 
 
-const char* ssid     = "wifi_ssid";                          //WiFi Name
-const char* password = "";                   					// WiFi Password
+// TinyGSM Client for Internet connection
+TinyGsmClient client(modem);
 
-#define uS_TO_S_FACTOR 1000000  //Conversion factor for micro seconds to seconds
-#define TIME_TO_SLEEP  900        //Time ESP32 will go to sleep (in seconds): 900 sec = 15 min
+#define uS_TO_S_FACTOR 1000000     /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP  900        /* Time ESP32 will go to sleep (in seconds) 3600 seconds = 1 hour */
 
-Adafruit_AM2320 am2320 = Adafruit_AM2320(); //declaration of am2320 sensor
+#define IP5306_ADDR          0x75
+#define IP5306_REG_SYS_CTL0  0x00
 
-float temperature = 0.0;
-float humidity = 0.0;
-
-const String baseUrl = "example.com/collector_page?";
-String url = "";
-
-//functions
-void print_wakeup_reason();
-
-
-void setup(){
-	Serial.begin(115200);
-	delay(1000); //Take some time to open up the Serial Monitor
-	
-	am2320.begin(); //open connection with am2320 sensor
-
-
-	//Print the wakeup reason for ESP32
-	print_wakeup_reason();
-
-	//Set timer
-	esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
-	Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) + " Seconds");
-
-	//connect to wifi
-	WiFi.begin(ssid, password);
-	int wifiCounter = 0;
-	while (WiFi.status() != WL_CONNECTED || wifiCounter < 30){
-		delay(500);
-		Serial.print(".");
-		wifiCounter++;
-	}
-
-	//read values from sensor
- 	temperature = am2320.readTemperature();
-	humidity = am2320.readHumidity();
-	url = "";
-	url = baseUrl + "t=" + String(temperature) + "&h=" + String(humidity); //GET HTTP string creation
-
-	//send data via HTTP GET
-	HTTPClient http;
-	http.begin(url);
-	int httpCode = http.GET();
-    if (httpCode > 0) { //Check for the returning code
-        String payload = http.getString();
-        Serial.println(httpCode);
-        Serial.println(payload);
-    }
-    else {
-      Serial.println("Error on HTTP request");
-    }
- 
-    http.end(); //Free the resources
-
-	//Go to sleep now
-	esp_deep_sleep_start();
+bool setPowerBoostKeepOn(int en){
+  I2CPower.beginTransmission(IP5306_ADDR);
+  I2CPower.write(IP5306_REG_SYS_CTL0);
+  if (en) {
+    I2CPower.write(0x37); // Set bit1: 1 enable 0 disable boost keep on
+  } else {
+    I2CPower.write(0x35); // 0x37 is default reg value
+  }
+  return I2CPower.endTransmission() == 0;
 }
 
+void setup() {
+  pinMode(9, OUTPUT);
+  digitalWrite(9, HIGH);
 
+  // Set serial monitor debugging window baud rate to 115200
+  SerialMon.begin(115200);
 
-void loop(){}
+  // Start I2C communication
+  I2CPower.begin(I2C_SDA, I2C_SCL, 400000);
 
+  // Keep power when running from battery
+  bool isOk = setPowerBoostKeepOn(1);
+  SerialMon.println(String("IP5306 KeepOn ") + (isOk ? "OK" : "FAIL"));
 
+  // Set modem reset, enable, power pins
+  pinMode(MODEM_PWKEY, OUTPUT);
+  pinMode(MODEM_RST, OUTPUT);
+  pinMode(MODEM_POWER_ON, OUTPUT);
+  digitalWrite(MODEM_PWKEY, LOW);
+  digitalWrite(MODEM_RST, HIGH);
+  digitalWrite(MODEM_POWER_ON, HIGH);
 
-//Function that prints the reason by which ESP32 has been awaken from sleep
-void print_wakeup_reason(){
-	esp_sleep_wakeup_cause_t wakeup_reason;
-	wakeup_reason = esp_sleep_get_wakeup_cause();
-	switch(wakeup_reason)
-	{
-		case 1  : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
-		case 2  : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
-		case 3  : Serial.println("Wakeup caused by timer"); break;
-		case 4  : Serial.println("Wakeup caused by touchpad"); break;
-		case 5  : Serial.println("Wakeup caused by ULP program"); break;
-		default : Serial.println("Wakeup was not caused by deep sleep"); break;
-	}
+  // Set GSM module baud rate and UART pins
+  SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
+  delay(3000);
+
+  // Restart SIM800 module, it takes quite some time
+  // To skip it, call init() instead of restart()
+  SerialMon.println("Initializing modem...");
+  modem.restart();
+  // use modem.init() if you don't need the complete restart
+
+  // Unlock your SIM card with a PIN if needed
+  if (strlen(simPIN) && modem.getSimStatus() != 3 ) {
+    modem.simUnlock(simPIN);
+  }
+  
+  //initialize dht sensor
+  dht.begin();
+
+  // Configure the wake up source as timer wake up  
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+}
+
+void loop() {
+  SerialMon.print("Connecting to APN: ");
+  SerialMon.print(apn);
+  if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+    SerialMon.println(" fail");
+  }
+  else {
+    SerialMon.println(" OK");
+    
+    SerialMon.print("Connecting to ");
+    SerialMon.print(server);
+    if (!client.connect(server, port)) {
+      SerialMon.println(" fail");
+    }
+    else {
+      SerialMon.println(" OK");
+
+      float temperature = dht.readTemperature();
+      float humidity = dht.readHumidity();
+      if (isnan(temperature) || isnan(humidity)) {
+        Serial.println(F("Failed to read from DHT sensor!"));
+        return;
+      }
+
+      SerialMon.println("Temperature: " + String(temperature) + " C --- Humidity: " + String(humidity));
+
+      // Making an HTTP POST request
+      SerialMon.println("Performing HTTP POST request...");
+      // Prepare your HTTP POST request data (Temperature in Celsius degrees)
+      String httpRequestData = "t=" + String(temperature)
+                             + "&h=" + String(humidity) + "";
+      // Prepare your HTTP POST request data (Temperature in Fahrenheit degrees)
+      //String httpRequestData = "api_key=" + apiKeyValue + "&value1=" + String(1.8 * bme.readTemperature() + 32)
+      //                       + "&value2=" + String(bme.readHumidity()) + "&value3=" + String(bme.readPressure()/100.0F) + "";
+          
+      // You can comment the httpRequestData variable above
+      // then, use the httpRequestData variable below (for testing purposes without the BME280 sensor)
+      //String httpRequestData = "api_key=tPmAT5Ab3j7F9&value1=24.75&value2=49.54&value3=1005.14";
+    
+      client.print(String("POST ") + resource + " HTTP/1.1\r\n");
+      client.print(String("Host: ") + server + "\r\n");
+      client.println("Connection: close");
+      client.println("Content-Type: application/x-www-form-urlencoded");
+      client.print("Content-Length: ");
+      client.println(httpRequestData.length());
+      client.println();
+      client.println(httpRequestData);
+
+      unsigned long timeout = millis();
+      while (client.connected() && millis() - timeout < 10000L) {
+        // Print available data (HTTP response from server)
+        while (client.available()) {
+          char c = client.read();
+          SerialMon.print(c);
+          timeout = millis();
+        }
+      }
+      SerialMon.println();
+    
+      // Close client and disconnect
+      client.stop();
+      SerialMon.println(F("Server disconnected"));
+      modem.gprsDisconnect();
+      SerialMon.println(F("GPRS disconnected"));
+    }
+  }
+  // Put ESP32 into deep sleep mode (with timer wake up)
+  esp_deep_sleep_start();
 }
